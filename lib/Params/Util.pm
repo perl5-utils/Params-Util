071a4bdec19a5475600f5abbbde5e719bc9751d1
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2011-04-20 07:15:19 +0000

    Fixed #67522
    git-svn-id: http://svn.ali.as/cpan/trunk/Params-Util@14154 88f4d9cd-8a04-0410-9d60-8f63309c3137

--- lib/Params/Util.pm
+++ lib/Params/Util.pm
@@ -801,7 +801,7 @@ L<Params::Validate>
 
 =head1 COPYRIGHT
 
-Copyright 2005 - 2010 Adam Kennedy.
+Copyright 2005 - 2011 Adam Kennedy.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
