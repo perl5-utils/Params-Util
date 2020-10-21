1395daac250f2c5b22a40ccdcf04ac9a674b48b9
Jens Rehsack <sno@netbsd.org>
2020-10-21 08:34:46 +0200

    Changes,lib/: prepare PROD-release
    Bump version and add noteworthy changes to do a PROD-Release after
    DEV-Release survided three weeks without significant surprises.
    
    Signed-off-by: Jens Rehsack <sno@netbsd.org>

--- lib/Params/Util.pm
+++ lib/Params/Util.pm
@@ -62,7 +62,7 @@ use parent qw{Exporter XSLoader};
 
 use Params::Util::PP qw();
 
-our $VERSION = '1.099_001';
+our $VERSION = '1.100';
 
 local $ENV{PERL_DL_NONLAZY} = 0 if $ENV{PERL_DL_NONLAZY};
 XSLoader::load("Params::Util", $VERSION) unless $ENV{PERL_PARAMS_UTIL_PP};
